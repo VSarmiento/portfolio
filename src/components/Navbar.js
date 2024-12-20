641aa4a15b0f97636b12a28e6cc740901d76c24d
VSarmiento <89476934+VSarmiento@users.noreply.github.com>
2024-12-19 22:28:21 -0800

    Added media to nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import github from '../assets/github-icon.png'
+import linkedin from '../assets/linkedin-icon.png'
 import '../styles/Navbar.css';
 
 const Navbar = ({ isOpen, toggleSidebar }) => {
@@ -13,11 +15,21 @@ const Navbar = ({ isOpen, toggleSidebar }) => {
 
       <div className={`sidebar ${isOpen ? 'open' : ''}`}>
         <div className="sidebar-content">
-          <nav className="nav-links">
+          <div className="social-links">
+            <a 
+              href="https://github.com/VSarmiento" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <img src={ github } alt="GitHub" />
+            </a>
+            <a 
+              href="https://www.linkedin.com/in/vahl-sarmiento/" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <img src={ linkedin } alt="LinkedIn" />
+            </a>
+         </div>
+         <nav className="nav-links">
             <Link to="/" className="nav-button">Home</Link>
-            <Link to="/about" className="nav-button">About</Link>
-            <Link to="/projects" className="nav-button">Projects</Link>
-            <Link to="/contact" className="contact-button">Contact</Link>
+           <Link to="/about" className="nav-button">About</Link>
+           <Link to="/projects" className="nav-button">Projects</Link>
+           <Link to="/contact" className="contact-button">Contact</Link>
           </nav>
         </div>
       </div>
